215fb9ef6881957853aff8aab4eaebbe2d01f1eb
ambroise <ambroise.berthe@ikomia.com>
2023-09-20 11:43:51 +0200

    set cache dir in algo directory

--- infer_detectron2_retinanet_process.py
+++ infer_detectron2_retinanet_process.py
@@ -84,6 +84,9 @@ class Retinanet(dataprocess.CObjectDetectionTask):
         # Get parameters :
         param = self.get_param_object()
 
+        # Set cache dir in the algorithm folder to simplify deployment
+        os.environ["FVCORE_CACHE"] = os.path.join(os.path.dirname(__file__), "models")
+
         # predictor
         if not self.loaded:
             print("Chargement du modèle")
